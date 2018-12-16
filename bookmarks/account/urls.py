ca2d9187d6330be5b4e2a336122dcb88e303f483
Krzysztof Formanowicz <formanowicz.k@gmail.com>
2018-12-16 11:42:12 +0100

    added password change

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     url(r'^login/$', auth_views.LoginView.as_view(), name='login'),
     url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
     url(r'^logout-then-login/$', auth_views.logout_then_login, name='logout_then_login'),
-    url(r'^$', views.dashboard, name='dashboard')
+    url(r'^$', views.dashboard, name='dashboard'),
+    url(r'^password-change/$', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    url(r'^password-change/done/$', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
 ]
