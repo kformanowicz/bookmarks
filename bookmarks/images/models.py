b069f670cf1676dc57bf71c8cec369ae1bc2950b
Krzysztof Formanowicz <formanowicz.k@gmail.com>
2019-06-29 19:22:38 +0400

    added image detail view

--- bookmarks/images/models.py
+++ bookmarks/images/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.urls import reverse
 from django.utils.text import slugify
 
 
@@ -17,6 +18,9 @@ class Image(models.Model):
         if not self.slug:
             self.slug = slugify(self.title)
             super(Image, self).save(force_insert, force_update, using, update_fields)
+
+    def get_absolute_url(self):
+        return reverse('images:detail', args=[self.id, self.slug])
 
     def __str__(self):
         return self.title
--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import ImageCreateForm
+from .models import Image
 
 
 @login_required
@@ -23,4 +24,14 @@ def image_create(request):
                   {
                       'section': 'images',
                       'form': form
+                  })
+
+
+def image_detail(request, id, slug):
+    image = get_object_or_404(Image, id=id, slug=slug)
+    return render(request,
+                  'images/image/detail.html',
+                  {
+                      'section': 'images',
+                      'image': image
                   })
