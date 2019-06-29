4526bc543ee9d3f9438e290918d7cf75e2869d1a
Krzysztof Formanowicz <formanowicz.k@gmail.com>
2019-06-29 15:39:49 +0400

    model save method overwritten for images

--- bookmarks/images/forms.py
+++ bookmarks/images/forms.py
@@ -1,3 +1,6 @@
+from urllib import request
+from django.core.files.base import ContentFile
+from django.utils.text import slugify
 from django import forms
 from .models import Image
 
@@ -17,3 +20,15 @@ class ImageCreateForm(forms.ModelForm):
         if extension not in valid_extensions:
             raise forms.ValidationError('Given URL address does not contain image in supported format.')
         return url
+
+    def save(self, force_insert=False, force_update=False, commit=True):
+        image = super(ImageCreateForm, self).save(commit=False)
+        image_url = self.cleaned_data['url']
+        image_name = '{}.{}'.format(slugify(image.title), image_url.rsplit('.', 1)[1].lower())
+
+        response = request.urlopen(image_url)
+        image.image.save(image_name, ContentFile(response.read()), save=False)
+
+        if commit:
+            image.save()
+        return image
